28d548044a504033e0278d3c2ac2a783f3dde2d9
A8Alumno02 <noemicanoconesa@gmail.com>
2026-09-17 13:37:57 +0200

    finish menu

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 // #region ---- Navegación Sidebar ----
 
-document.addEventListener(
+const menuBtn = document.getElementById('menuBtn');
+const menuList = document.getElementById('menuList');
 
-);
+menuBtn.addEventListener('click', () => {
+    const isExpanded = menuList.classList.toggle('active');
+    // Actualiza el estado de accesibilidad
+    menuBtn.setAttribute('aria-expanded', isExpanded);
+});
 
+document.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && menuList.classList.contains('active')) {
+        menuList.classList.remove('active');
+        menuBtn.setAttribute('aria-expanded', 'false');
+        menuBtn.focus();
+    }
+});
 
 // #endregion
